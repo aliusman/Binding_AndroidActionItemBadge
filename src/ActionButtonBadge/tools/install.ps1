7aa74d5ccd8abdb6fbf0314653b5db365c124ed5
Arnstein Johansen <arnstej@gmail.com>
2016-03-07 12:06:20 +0100

    Added resources

--- src/ActionButtonBadge/tools/install.ps1
+++ src/ActionButtonBadge/tools/install.ps1
@@ -6,3 +6,22 @@ param($installPath, $toolsPath, $package, $project)
 # $toolsPath is the path to the tools directory in the folder where the package is installed.
 # $package is a reference to the package object.
 # $project is a reference to the project the package was installed to.
+
+$deployLayoutTarget = "$project\Resources\layout\"
+$deployDrawableTarget = "$project\Resources\drawable"
+
+$deployLayoutSource = join-path $installPath 'content/layout'
+$deployDrawableSource = join-path $installPath 'content/drawable'
+
+
+if (!(test-path $deployLayoutTarget)) {
+	mkdir $deployLayoutTarget
+}
+
+if (!(test-path $deployDrawableTarget)) {
+	mkdir $deployDrawableTarget
+}
+
+Copy-Item "$deployLayoutSource/*" $deployLayoutTarget -Recurse -Force
+Copy-Item "$deployDrawableSource/*" $deployDrawableTarget -Recurse -Force
+
